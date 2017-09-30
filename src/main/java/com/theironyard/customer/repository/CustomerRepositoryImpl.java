bf3d93d4eeffdaf8ae676c7777a54995ccecd279
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-30 11:03:57 -0500

    add model and repository package

---
+++ src/main/java/com/theironyard/customer/repository/CustomerRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.theironyard.customer.repository;
+
+import com.sun.tools.javac.util.List;
+import com.theironyard.customer.model.Customer;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+
+    @Override
+    public void add(Customer customer) {
+
+    }
+
+    @Override
+    public void update(Customer customer) {
+
+    }
+
+    @Override
+    public Customer getById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Customer> get() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
