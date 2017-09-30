f18bbcaeac4f006dfb826a1e82e3342f2349de7f
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-30 11:37:10 -0500

    creating customer service methods

--- src/main/java/com/theironyard/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/theironyard/customer/service/CustomerServiceImpl.java
@@ -2,16 +2,27 @@ package com.theironyard.customer.service;
 
 import com.sun.tools.javac.util.List;
 import com.theironyard.customer.model.Customer;
+import com.theironyard.customer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 public class CustomerServiceImpl implements CustomerService {
 
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+    @Transactional
     @Override
     public void add(Customer customer) {
+        customerRepository.add(customer);
 
     }
 
+    @Transactional
     @Override
     public void update(Customer customer) {
+        customerRepository.update(customer);
 
     }
 
@@ -25,6 +36,8 @@ public class CustomerServiceImpl implements CustomerService {
         return null;
     }
 
+
+    @Transactional
     @Override
     public void delete(int id) {
 
